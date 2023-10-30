b8c91b2c58eeac2c751cc9ad027466d6f9e1703c
Ahmed Esmail <esmailahmed511@gmail.com>
2023-10-30 23:41:52 +0200

    Update colors.dart added the colors

---
+++ lib/shared/styles/colors.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+const primaryColor = Color(0xFF28D8AE);
+const foregroundColor = Color(0xFFF3F2F7);
+const backgroundColor = Color(0xFFFAFAFA);
+const primaryHoverColor = Color(0xFF30A79F);
+const mutedColor = Color(0xFFC8C5C5);
+const secondaryColor = Color(0xFFD19F54);
+const flatColor = Color(0xFF28D8AE);
+const blackColor = Color(0xFF1F292F);
+const announcementColor = Color(0xFFFCBE45);
+const dangerColor = Color(0xFFFA5057);
+const whiteColor = Color(0xFFFFFFFF);
+const midGrayColor = Color(0xFFEFEFEF);
